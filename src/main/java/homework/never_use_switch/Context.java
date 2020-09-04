017e660e1da3212a4f54aaa5c505e5f11c2b12ae
artem-chemezov <chemezov.artyom@gmail.com>
2020-09-04 11:19:07 +0300

    fix

--- src/main/java/homework/never_use_switch/Context.java
+++ src/main/java/homework/never_use_switch/Context.java
@@ -1,16 +1,17 @@
 package homework.never_use_switch;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class Context {
-    private HashMap<Integer, Consumer<MailInfo>> context = new HashMap<>();
+    private Map<Integer, Consumer<MailInfo>> context = new HashMap<>();
 
     public void registerMethod(int type, Consumer<MailInfo> function){
         context.put(type, function);
     }
 
     public void call(Integer type, MailInfo mailInfo){
-        context.get(type).accept(mailInfo);
+        context.getOrDefault(type, (mailInfo1) -> {throw new IllegalStateException();}).accept(mailInfo);
     }
 }
